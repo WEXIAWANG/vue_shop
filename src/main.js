0d6837bfc1d2ea8c17eef6334aca879a7797d0c1
WEXIA <501325190@qq.com>
2021-06-17 10:10:42 +0800

    finish log

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/font_29zz55l67l/iconfont.css'
+import axios from "axios";
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://www.ysqorz.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 
 new Vue({
   router,
