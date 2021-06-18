bb7c46609b6c9985c659795e26d74bf37411e584
WEXIA <501325190@qq.com>
2021-06-18 19:38:06 +0800

    finish user

--- src/main.js
+++ src/main.js
@@ -7,16 +7,20 @@ import './plugins/element.js'
 import './assets/css/global.css'
 // 导入字体图标
 import './assets/font_29zz55l67l/iconfont.css'
-import axios from "axios";
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = 'http://www.ysqorz.top:8888/api/private/v1/'
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
-
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,51 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login'
-import Home from "@/components/Home";
+import Home from '@/components/Home'
+import Welcome from '@/components/Welcome'
+import Users from '@/components/User/Users'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path:'/',redirect:'/login'
+    path: '/',
+    redirect: '/login',
   },
   {
-    path:'/login',component:Login
+    path: '/login',
+    component: Login,
   },
   {
-    path:'/home',component: Home
-  }
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      {
+        path: '/welcome',
+        component: Welcome,
+      },
+      {
+        path: '/users',
+        component: Users,
+      },
+    ],
+  },
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
 })
 
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to, from, next) => {
   // to 将要访问的 路径
   // from 代表从哪个路径跳转而来
   // next 是一个函数，表示放行
   //  next（）放行  next（‘/login’） 强制跳转
-  if(to.path === '/login') return next()
+  if (to.path === '/login') return next()
   // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+  if (!tokenStr) return next('/login')
   next()
 })
 
